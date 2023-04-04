a61eaa3962b5ea491d035caca3d2cddb371d18f1
Brochu <alexbrochu1@gmail.com>
2023-04-04 18:07:37 -0400

    [SETUP] Prepare main for different sections of the course

--- CourseTests.cpp
+++ CourseTests.cpp
@@ -1,7 +1,8 @@
 #include <cstdio>
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     printf("[MAIN] Hello from main.\n");
+
+    //TODO: Separate the different sections of the course in different files
     return 0;
 }
