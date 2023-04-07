7a24552502161f3e28a40d6386030a0cbc1bd024
Brochu <alexbrochu1@gmail.com>
2023-04-07 02:55:35 -0400

    Placeholder for first sort algo

--- Sort.h
+++ Sort.h
@@ -5,6 +5,9 @@
 
 namespace sort {
 
+    inline void bubble_sort() {
+    }
+
     inline void run_test() {
         printf("[SORT] Running sorting tests\n");
         std::array<int, 10> arr {3, -10, 24, 78, 7, 99, 36, 88, 41, 0};
