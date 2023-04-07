f7e8cabf0451f2798d7d9ecc54bb2b209109cce3
Brochu <alexbrochu1@gmail.com>
2023-04-07 02:54:13 -0400

    Added new file for sorting tests

---
+++ Sort.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include <cstdio>
+#include <array>
+
+namespace sort {
+
+    inline void run_test() {
+        printf("[SORT] Running sorting tests\n");
+        std::array<int, 10> arr {3, -10, 24, 78, 7, 99, 36, 88, 41, 0};
+    }
+    
+}
